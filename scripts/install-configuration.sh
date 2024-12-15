bf49711f7313d95d736fc413fac1fe9ca20f5c99
Matt <MSzturc@googlemail.com>
2024-12-16 00:01:24 +0100

    fixed macro folders

--- scripts/install-configuration.sh
+++ scripts/install-configuration.sh
@@ -33,14 +33,20 @@ downloadConfiguration() {
 }
 
 installConfiguration() {
-    info Installation of THE100-Configruation...
+    info "Installation of THE100 Configuration..."
+    
     # Symlink THE100 Configuration (read-only git repository) to the user's config directory
+    info "Creating symbolic links for configuration directories..."
     for dir in config macros scripts; do
+        info "Linking directory: $dir"
         ln -fsn ${THE100_CONFIG_PATH}/$dir ${KLIPPER_CONFIG_PATH}/$dir
     done
 
     # CHMOD the scripts to be sure they are all executables (Git should keep the modes on files but it's to be sure)
+    info "Ensuring scripts are executable..."
     chmod +x ${THE100_CONFIG_PATH}/scripts/install-configuration.sh
+
+    info "Installation of THE100 Configuration completed successfully!"
 }
 
 
