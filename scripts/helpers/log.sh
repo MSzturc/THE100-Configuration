ddc41ef416b2535504b6db7770bd012c4998084c
Matt <MSzturc@googlemail.com>
2024-12-16 16:18:02 +0100

    added log dir

--- scripts/helpers/log.sh
+++ scripts/helpers/log.sh
@@ -3,7 +3,6 @@
 # Global variable to store the last used timestamp
 LAST_TIMESTAMP=""
 
-
 # Logging function with precise alignment of log levels
 log() {
     LOG_FILE="$(user_dir)/logs/theos.log"
@@ -23,27 +22,32 @@ log() {
     local timestamp_width=21
     local level_width=10
 
-    # Check if the timestamp has changed
+    # Append log to the log file with the same format as console output
+    mkdir -p "$(user_dir)/logs"
     if [ "$timestamp" != "$LAST_TIMESTAMP" ]; then
-        # Print timestamp and log level
-        printf "%-${timestamp_width}s %b%-${level_width}s\e[0m %s (%s)\n" "[$timestamp]" "$color" "[$level]" "$text" "$caller"
+        printf "%-${timestamp_width}s [%s] %-${level_width}s %s (%s)\n" "[$timestamp]" "$level" "[$level]" "$text" "$caller" >> "$LOG_FILE"
         LAST_TIMESTAMP="$timestamp"
     else
-        # Print only spaces for the timestamp and align log level
-        printf "%-${timestamp_width}s %b%-${level_width}s\e[0m %s (%s)\n" " " "$color" "[$level]" "$text" "$caller"
+        printf "%-${timestamp_width}s [%s] %-${level_width}s %s (%s)\n" " " "$level" "[$level]" "$text" "$caller" >> "$LOG_FILE"
     fi
 
-    # Append log to the log file
-    mkdir -p "$(user_dir)/logs"
-    printf "[%s] [%s] %s (%s)\n" "$timestamp" "$level" "$text" "$caller" >> "$LOG_FILE"
+    # Print to console only if not DEBUG or debugging is active
+    if [ "$level" != "DEBUG" ] || [ -f "$(user_dir)/logs/debugging.active" ]; then
+        if [ "$timestamp" != "$LAST_TIMESTAMP" ]; then
+            # Print timestamp and log level
+            printf "%-${timestamp_width}s %b%-${level_width}s\e[0m %s (%s)\n" "[$timestamp]" "$color" "[$level]" "$text" "$caller"
+            LAST_TIMESTAMP="$timestamp"
+        else
+            # Print only spaces for the timestamp and align log level
+            printf "%-${timestamp_width}s %b%-${level_width}s\e[0m %s (%s)\n" " " "$color" "[$level]" "$text" "$caller"
+        fi
+    fi
 }
 
 # Individual functions for each log level
 debug() {
     DEBUGGING_FILE="$(user_dir)/logs/debugging.active"
-    if [ -f "$DEBUGGING_FILE" ]; then
-        log "DEBUG" "\e[36m" "$1"  # Cyan
-    fi
+    log "DEBUG" "\e[36m" "$1"  # Cyan
 }
 
 info() {
--- scripts/install-configuration.sh
+++ scripts/install-configuration.sh
@@ -19,7 +19,7 @@ THE100_CONFIG_REPOSITORY="https://github.com/MSzturc/THE100-Configuration.git"
 # Branch from MSzturc/THE100-Configuration repo to use during install (default: main)
 THE100_CONFIG_BRANCH="main"
 
-downloadConfiguration() {
+download_configuration() {
     
     if [ -d "$THE100_CONFIG_PATH" ]; then
         info "Configuration repository already found locally at $THE100_CONFIG_PATH. Skipping download."
@@ -34,7 +34,7 @@ downloadConfiguration() {
     fi
 }
 
-installConfiguration() {
+install_configuration() {
     info "Installation of THE100 Configuration..."
     
     # Symlink THE100 Configuration (read-only git repository) to the user's config directory
@@ -66,12 +66,49 @@ install_hooks()
     fi
 }
 
+# This function checks if a 'logs' directory exists in the user's home directory.
+# If it doesn't exist, the function creates the directory and sets its permissions to allow 
+# read and write access for all users. Additionally, it creates an empty 'theos.log' file 
+# in the directory with the same permissions.
+install_logs() {
+
+    # Path to the logs directory
+    local logs_dir="$(user_dir)/logs"
+    local log_file="$logs_dir/theos.log"
+
+    # Check if the 'logs' folder exists
+    if [ ! -d "$logs_dir" ]; then
+        info "Creating folder: $logs_dir"
+        mkdir "$logs_dir"
+    else
+        debug "Folder already exists: $logs_dir"
+    fi
+
+    # Set permissions: read and write for all users (folder and future files)
+    chmod 777 "$logs_dir"
+    info "Permissions for folder $logs_dir set: read and write for all users."
+
+    # Check if the file 'theos.log' exists, otherwise create it
+    if [ ! -f "$log_file" ]; then
+        debug "Creating file: $log_file"
+        touch "$log_file"
+    else
+        debug "File already exists: $log_file"
+    fi
+
+    # Set permissions: read and write for all users for the file
+    chmod 666 "$log_file"
+    info "Permissions for file $log_file set: read and write for all users."
+}
+
+
 preflight_checks() {
     ensure_not_root
     is_klipper_installed
 }
 
 preflight_checks
-downloadConfiguration
-installConfiguration
+download_configuration
+install_configuration
+install_logs
 install_hooks
