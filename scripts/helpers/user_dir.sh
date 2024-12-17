91b32e5526602681a838c8cfcd4554033e00fc1d
Matt <MSzturc@googlemail.com>
2024-12-17 18:49:02 +0100

    added debugging code

--- scripts/helpers/user_dir.sh
+++ scripts/helpers/user_dir.sh
@@ -7,16 +7,19 @@ user_dir()
 
     # Check if BASE_USER environment variable is set
     if [[ -n "$BASE_USER" ]]; then
-        # Use the home directory of the user specified in BASE_USER
+        echo "BASE_USER ist gesetzt auf '$BASE_USER'."
         home_dir=$(getent passwd "$BASE_USER" | cut -d: -f6)
+        echo "Home-Verzeichnis für BASE_USER: $home_dir"
     elif [[ -n "$SUDO_USER" ]]; then
-        # If the script is run with sudo, use the $SUDO_USER variable
+        echo "SUDO_USER ist gesetzt auf '$SUDO_USER'."
         home_dir=$(getent passwd "$SUDO_USER" | cut -d: -f6)
+        echo "Home-Verzeichnis für SUDO_USER: $home_dir"
     else
-        # Use the home directory of the current user
+        echo "Weder BASE_USER noch SUDO_USER sind gesetzt. Verwende aktuelles HOME: $HOME"
         home_dir="$HOME"
     fi
 
-    # Print the home directory
+    # Print the final home directory
+    echo "Verwendetes Home-Verzeichnis: $home_dir"
     echo "$home_dir"
 }
