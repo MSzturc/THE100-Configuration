e79c090a4f8c871993f9f56ef03df6b5bd211d23
Matt <MSzturc@googlemail.com>
2024-12-17 14:09:38 +0100

    purge log on run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+rm -rf ~/logs/theos.log
+touch ~/logs/theos.log
+chown pi:pi ~/logs/theos.log
+chmod 666 ~/logs/theos.log
 git reset cd8a284 --hard
 git pull
 ./scripts/install-configuration.sh 
