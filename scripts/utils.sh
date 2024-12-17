6beda070cce8861b925e8183181dc891efa16b2a
Matt <MSzturc@googlemail.com>
2024-12-17 15:27:14 +0100

    moved ensure_moonraker_permissions to utils

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -11,6 +11,9 @@ source "$SCRIPT_DIR"/helpers/current_user.sh
 # Where to clone THE100-Configuration repository
 THE100_CONFIG_PATH="$(user_dir)/THE100-Configuration"
 
+# Where the user Klipper config is located
+PRINTER_DATA_PATH="$(user_dir)/printer_data"
+
 # Function to ensure the script is NOT run as root.
 ensure_not_root() {
     if [ "$EUID" -eq 0 ]; then
@@ -59,4 +62,52 @@ restart_klipper_service() {
         error "Failed to restart the Klipper service."
         exit 1
     fi
+}
+
+ensure_moonraker_permissions() {
+    local file_path="${PRINTER_DATA_PATH}/moonraker.asvc"
+
+    info "Authorized Moonraker to manage Services..."
+
+    # List of services that Moonraker should manage
+    local elements=(
+        "klipper_mcu"          
+        "webcamd"                
+        "MoonCord"               
+        "KlipperScreen"          
+        "moonraker-telegram-bot" 
+        "sonar"                  
+        "crowsnest"               
+    )
+
+    # TODO: Define THE100-Configuration as managed service
+
+    # Write the service names to the file, one per line
+    {
+        for element in "${elements[@]}"; do
+            echo "$element"
+        done
+    } > "$file_path"
+    
+    # Check if writing to the file was successful
+    if [ $? -ne 0 ]; then
+        error "Failed to write to file '$file_path'. Ensure the path is correct and writable."
+        exit 1
+    fi
+
+    # Set file permissions to 644 (readable by all, writable only by the owner)
+    chmod 644 "$file_path"
+    if [ $? -ne 0 ]; then
+        error "Failed to set permissions on '$file_path'."
+        exit 1
+    fi
+
+    # Set the file owner to user 'pi' and group 'pi'
+    chown pi:pi "$file_path"
+    if [ $? -ne 0 ]; then
+        error "Failed to change ownership of '$file_path'."
+        exit 1
+    fi
+
+    info "Successfully updated '$file_path'"
 }
