f551f82620ad383a308f99b5f6e2d9b48f2ecb23
Matt <MSzturc@googlemail.com>
2024-12-18 12:33:25 +0100

    better code documentation

--- scripts/helpers/current_user.sh
+++ scripts/helpers/current_user.sh
@@ -1,17 +1,21 @@
-# Function to determine the current user running the script.
-# If the script is run as root:
-#   - It checks the SUDO_USER environment variable to identify the original user.
-#   - If SUDO_USER is root, it overrides it with "pi."
-#   - If SUDO_USER is not set, it assumes the user is "root."
-# If the script is not run as root:
-#   - It echoes the value of the USER environment variable, which represents the current logged-in user.
+# Determines the current user based on the context in which the script is executed.
+# - If the script is run with sudo:
+#   - It checks if BASE_USER is set and uses it if available.
+#   - Otherwise, it evaluates the SUDO_USER environment variable:
+#     - If SUDO_USER is 'root', it defaults to 'pi' to address specific use cases
+#       (e.g., CustomPIOS image-building processes starting as root).
+#     - Otherwise, it returns the value of SUDO_USER.
+# - If the script is run as root:
+#   - Neither BASE_USER nor SUDO_USER is set, it defaults to 'root.'
+# - If the script is not run as root, it returns the current logged-in user ($USER).
 current_user() {
     if [ "$EUID" -eq 0 ]; then
         # If BASE_USER is set, use it
         if [ -n "$BASE_USER" ]; then
             echo "$BASE_USER"
-        # Otherwise, check for SUDO_USER
+        # Otherwise, check the SUDO_USER variable
         elif [ -n "$SUDO_USER" ]; then
+            # Handle specific case where SUDO_USER is 'root'
             if [ "$SUDO_USER" == "root" ]; then
                 echo "pi"
             else
@@ -21,7 +25,7 @@ current_user() {
             echo "root"
         fi
     else
-        # If not root, just return the current user
+        # If not run as root, return the current logged-in user
         echo "$USER"
     fi
 }
--- scripts/helpers/user_dir.sh
+++ scripts/helpers/user_dir.sh
@@ -1,17 +1,24 @@
 # This function retrieves the home directory of the current user.
-# - If the script is run with sudo, and SUDO_USER=root, it overrides SUDO_USER with 'pi'.
-# - If the BASE_USER variable is set, it determines the home directory based on BASE_USER.
-# - Otherwise, it uses the home directory of the current user ($HOME).
+# - If the script is executed with sudo and SUDO_USER is set to 'root',
+#   it overrides SUDO_USER with 'pi' to avoid issues.
+# - If the BASE_USER environment variable is set, the home directory
+#   is determined based on BASE_USER.
+# - Otherwise, it falls back to the home directory of the current user ($HOME).
 user_dir()
 {
     local home_dir
 
-    # Wenn SUDO_USER=root, setze SUDO_USER=pi
+    # CustomPIOS starts the image-building process as 'root',
+    # which causes SUDO_USER to also be 'root', creating a potential issue.
+    # As a workaround, we set SUDO_USER to 'pi' if it's 'root'.
+    # This is not a concern since the final step of the image-building
+    # process disables the root user, meaning this case won't occur
+    # during normal usage.
     if [[ "$SUDO_USER" == "root" ]]; then
         SUDO_USER="pi"
     fi
 
-    # Check if BASE_USER environment variable is set
+    # Check if the BASE_USER environment variable is set
     if [[ -n "$BASE_USER" ]]; then
         home_dir=$(getent passwd "$BASE_USER" | cut -d: -f6)
     elif [[ -n "$SUDO_USER" ]]; then
